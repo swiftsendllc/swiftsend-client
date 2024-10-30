97958b18c72e30c8cf2a87bf62cacaa20ed0d641
arijit <arijitchhatui.me@gmail.com>
2024-10-30 20:16:02 +0530

    fix

--- app/[username]/profile/components/EditProfile.tsx
+++ app/[username]/profile/components/EditProfile.tsx
@@ -106,7 +106,7 @@ export default function EditProfilePage() {
         alignItems="center"
         mb={2}
       >
-        <IconButton href="/account" LinkComponent={Link}>
+        <IconButton href={`/${user.username}`} LinkComponent={Link}>
           <ArrowBackIcon />
         </IconButton>
         <Typography alignContent="center" alignItems="center" variant="h6">
